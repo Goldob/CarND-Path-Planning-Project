0ad04811bdec2355acfab4cd0f02d50bedb51e76
Adam Gotlib <gotlib.adam+dev@gmail.com>
2019-04-30 17:19:25 +0200

    Drive along the initial lane with constant velocity

--- src/main.cpp
+++ src/main.cpp
@@ -63,12 +63,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -80,11 +80,11 @@ int main() {
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
           auto previous_path_y = j[1]["previous_path_y"];
-          // Previous path's end s and d values 
+          // Previous path's end s and d values
           double end_path_s = j[1]["end_path_s"];
           double end_path_d = j[1]["end_path_d"];
 
-          // Sensor Fusion Data, a list of all other cars on the same side 
+          // Sensor Fusion Data, a list of all other cars on the same side
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
@@ -92,6 +92,14 @@ int main() {
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
+
+		  double s_inc = 0.4;
+		  for (int i = 0; i < 50; i++)
+		  {
+			  auto p = getXY(car_s + i*s_inc, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			  next_x_vals.push_back(p[0]);
+			  next_y_vals.push_back(p[1]);
+		  }
 
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
@@ -131,6 +139,6 @@ int main() {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
-  
+
   h.run();
 }
