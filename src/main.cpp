342a53f347074501b50b127772f78fdb59ff01cc
Adam Gotlib <gotlib.adam+dev@gmail.com>
2019-06-19 18:53:25 +0200

    Drive smoothly on the middle lane

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -58,7 +59,6 @@ int main() {
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
-
       auto s = hasData(data);
 
       if (s != "") {
@@ -90,22 +90,97 @@ int main() {
 
           json msgJson;
 
+		  const double timestep = 0.02;
+
+		  int target_lane = 1;
+		  double target_velocity = 20.0;
+
+		  vector<double> anchor_x_vals;
+		  vector<double> anchor_y_vals;
+
+		  double ref_x, ref_y, ref_x_prev, ref_y_prev, ref_yaw;
+		  int N_prev = previous_path_x.size();
+		  if (N_prev >= 2) {
+			  ref_x_prev = previous_path_x[N_prev-2];
+			  ref_x = previous_path_x[N_prev-1];
+
+			  ref_y_prev = previous_path_y[N_prev-2];
+			  ref_y = previous_path_y[N_prev-1];
+
+			  ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+		  } else {
+			  ref_x_prev = car_x - cos(car_yaw);
+			  ref_x = car_x;
+
+			  ref_y_prev = car_y - sin(car_yaw);
+			  ref_y = car_y;
+
+			  ref_yaw = deg2rad(car_yaw);
+		  }
+
+		  anchor_x_vals.push_back(ref_x_prev);
+		  anchor_x_vals.push_back(ref_x);
+
+		  anchor_y_vals.push_back(ref_y_prev);
+		  anchor_y_vals.push_back(ref_y);
+
+		  auto ref_frenet = getFrenet(ref_x, ref_y, ref_yaw, map_waypoints_x, map_waypoints_y);
+		  double anchor_s_inc = 30.0;
+		  for (int i = 1; i <= 3; i++) {
+			  double s = ref_frenet[0] + i * anchor_s_inc;
+			  double d = 2 + 4 * target_lane;
+
+			  auto xy = getXY(s, d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			  anchor_x_vals.push_back(xy[0]);
+			  anchor_y_vals.push_back(xy[1]);
+		  }
+
+		  // Transform anchor points to the car's coordinate frame
+		  for (int i = 0; i < anchor_x_vals.size(); i++) {
+			  double x_diff = anchor_x_vals[i] - ref_x;
+			  double y_diff = anchor_y_vals[i] - ref_y;
+
+			  anchor_x_vals[i] = x_diff * cos(-ref_yaw) - y_diff * sin(-ref_yaw);
+			  anchor_y_vals[i] = x_diff * sin(-ref_yaw) + y_diff * cos(-ref_yaw);
+		  }
+
+		  tk::spline s;
+		  s.set_points(anchor_x_vals, anchor_y_vals);
+
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
-		  double s_inc = 0.4;
-		  for (int i = 0; i < 50; i++)
-		  {
-			  auto p = getXY(car_s + i*s_inc, car_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-			  next_x_vals.push_back(p[0]);
-			  next_y_vals.push_back(p[1]);
+		  // Start with all of the previous path points from last time
+		  for (int i = 0; i < previous_path_x.size(); i++) {
+			  next_x_vals.push_back(previous_path_x[i]);
+			  next_y_vals.push_back(previous_path_y[i]);
 		  }
 
-          /**
-           * TODO: define a path made up of (x,y) points that the car will visit
-           *   sequentially every .02 seconds
-           */
+		  // Calculate how to break up spline points so that we travel at our desired reference velocity
+		  double target_x = 30.0;
+		  double target_y = s(target_x);
+		  double target_d = distance(target_x, target_y, 0, 0);
 
+		  double s_inc = target_velocity * timestep;
+		  double N = target_d / s_inc;
+
+		  // Generate future path points
+		  for (int i = 1; i < 50 - previous_path_x.size(); i++)
+		  {
+			  double x = i * target_x / N;
+			  double y = s(x);
+
+			  // Transform current point back to the global coordinate frame
+
+			  double global_x = x * cos(ref_yaw) - y * sin(ref_yaw);
+			  double global_y = x * sin(ref_yaw) + y * cos(ref_yaw);
+
+			  global_x += ref_x;
+			  global_y += ref_y;
+
+			  next_x_vals.push_back(global_x);
+			  next_y_vals.push_back(global_y);
+		  }
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
